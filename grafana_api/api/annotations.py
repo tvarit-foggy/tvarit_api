dca08ecbb1dbbf061d0abc4c701e82107c8384d7
DheerajDang <dang@voereir.com>
2019-06-27 17:09:31 +0530

    Alerting API

--- grafana_api/api/annotations.py
+++ grafana_api/api/annotations.py
@@ -2,17 +2,48 @@ from .base import Base
 
 
 class Annotations(Base):
-
     def __init__(self, api):
         super(Annotations, self).__init__(api)
         self.api = api
 
-    def list_annotations(self):
+    def list_annotations(self, **kwargs):
         """
         Method to list all annotations
         :return: response
         """
         path = '/annotations'
+        params = []
+        if kwargs.get('from'):
+            params.append('from=%s' % kwargs['from'])
+
+        if kwargs.get('to'):
+            params.append('to=%s' % kwargs['to'])
+
+        if kwargs.get('limit'):
+            params.append('limit=%s' % kwargs['limit'])
+
+        if kwargs.get('alertId'):
+            params.append('alertId=%s' % kwargs['alertId'])
+
+        if kwargs.get('dashboardId'):
+            params.append('dashboardId=%s' % kwargs['dashboardId'])
+
+        if kwargs.get('panelId'):
+            params.append('panelId=%s' % kwargs['panelId'])
+
+        if kwargs.get('userId'):
+            params.append('userId=%s' % kwargs['userId'])
+
+        if kwargs.get('type'):
+            params.append('type=%s' % kwargs['type'])
+
+        if kwargs.get('tags'):
+            params.extend(['tags=%s' % v for v in v kwargs['tags']])
+
+        if params:
+            path += '?'
+            path += '&'.join(params)
+
         r = self.api.GET(path)
         return r
 
