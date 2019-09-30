b7ef89ecb3bc675866d5d18ae26943d3ae2c1c3a
Kamal Galrani <kamal.galrani@tvarit.com>
2019-09-30 18:54:40 +0530

    fixes

--- tvarit_api/resources/job.py
+++ tvarit_api/resources/job.py
@@ -7,21 +7,21 @@ class Job(Base):
         self.api = api
 
     def run_job(self, job):
-    	endpoint = "/jobs"
+        endpoint = "/jobs"
         return self.api.POST(endpoint, json=job)
 
     def rerun_job(self, job_id):
-    	endpoint = "/{}/rerun".format(job_id)
+        endpoint = "/{}/rerun".format(job_id)
         return self.api.POST(endpoint)
 
     def abort_job(self, job_id):
-    	endpoint = "/{}/abort".format(job_id)
+        endpoint = "/{}/abort".format(job_id)
         return self.api.POST(endpoint)
 
     def job_status(self, job_id):
-    	endpoint = "/{}/status".format(job_id)
+        endpoint = "/{}/status".format(job_id)
         return self.api.POST(endpoint)
 
     def job_logs(self, job_id):
-    	endpoint = "/{}/logs".format(job_id)
+        endpoint = "/{}/logs".format(job_id)
         return self.api.POST(endpoint)
